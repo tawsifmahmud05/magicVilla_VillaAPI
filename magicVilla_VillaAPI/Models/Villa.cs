eb819c458e083af666cde98194f5c4f49e3d2ebc
tawsifmahmud05 <tawsifmahmud05@gmail.com>
2024-07-25 17:51:05 +0600

    logger_31

---
+++ magicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,11 @@
+﻿namespace magicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
